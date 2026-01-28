a648564653690240d1e5599a5b1a1878659f55ee
Pharadorn-K <pharadonkumwong@gmail.com>
2026-01-28 16:34:12 +0700

    Update office

--- frontend/public/js/sidebar.js
+++ frontend/public/js/sidebar.js
@@ -3,15 +3,15 @@ export function renderSidebar(role) {
   return `
     <nav class="sidebar-nav">
       <div class="sidebar-section">
-        <a data-page="home"><i class="fa-solid fa-arrow-right-to-bracket"></i><span>Home</span></a>
-        <a data-page="production">🏭 <span>Production</span></a>
-        <a data-page="maintenance">🛠 <span>Maintenance</span></a>
+        <a data-page="home"><i class="fa-solid fa-display"></i></i> <span>Home</span></a>
+        <a data-page="production"><i class="fa-solid fa-industry"></i> <span>Production</span></a>
+        <a data-page="maintenance"><i class="fa-solid fa-screwdriver-wrench"></i> <span>Maintenance</span></a>
 
       </div>
 
       ${role === 'admin' ? `
       <div class="sidebar-section">
-        <a data-page="admin">⚙ <span>Admin</span></a>
+        <a data-page="admin"><i class="fa-solid fa-laptop-code"></i><span>    Admin</span></a>
       </div>` : ''}
     </nav>
   `;
